0ab272a614e5bbab332266ac8f491e4d61cd89c4
Joe Swan <114914+TheAllSeeingPie@users.noreply.github.com>
2024-12-29 10:35:54 +0000

    Added activities endpoint and refactored

--- app.py
+++ app.py
@@ -4,13 +4,11 @@ from datetime import date, timedelta
 
 app = Flask(__name__)
 
-@app.route('/api/summary', methods=['POST'])
-def api_summary():
+def login_and_get_client():
     # Parse request JSON for credentials and parameters
     req_data = request.get_json()
     email = req_data.get('email')
     password = req_data.get('password')
-    period = req_data.get('period', 31)
     
     if not email or not password:
         return jsonify({"error": "Email and password are required"}), 400
@@ -19,11 +17,35 @@ def api_summary():
         garmin = garminconnect.Garmin(email, password)
         garmin.login()
         
+        return garmin
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/summary', methods=['POST'])
+def api_summary():
+    req_data = request.get_json()
+    period = req_data.get('period', 30)
+    
+    try:
+        garmin = login_and_get_client();
         data = []
 
-        for x in range(period):
+        for x in range(period+1):
             d = (date.today() - timedelta(days=x)).isoformat()
             data.append(garmin.get_stats_and_body(d))
+        
+        return jsonify(data)
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
+
+@app.route('/api/activities', methods=['POST'])
+def api_activities():
+    req_data = request.get_json()
+    period = req_data.get('period', 30)
+    
+    try:
+        garmin = login_and_get_client();
+        data = garmin.get_activities_by_date(date.today() - timedelta(period), date.today())
         
         return jsonify(data)
     except Exception as e:
